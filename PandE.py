35570612ebbfe8e423b2ed76102cc68b8e5a7650
ecyht2 <ecyht2@nottingham.edu.my>
2022-03-28 10:02:28 +0800

    Added PandE

---
+++ PandE.py
@@ -0,0 +1,18 @@
+import math
+from math import pi
+
+def par_res(resistors, *res):
+    if type(resistors) == list:
+        sum(resisstors**-1)**-1
+
+def imp_cap(capacitor, freq):
+    return 1/(2*pi*freq*capacitor*1j)
+
+def imp_ind(ind, freq):
+    return 2*pi*freq*ind*1j
+
+def imp_cap_rev(imp, freq):
+    return 1/(2*pi*freq*capacitor*1j)
+
+def imp_ind(ind, freq):
+    return 2*pi*freq*ind*1j
