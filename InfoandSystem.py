573266a4c3ad7ecea48f8e94f6f790c4574bfbe5
ecyht2 <ecyht2@nottingham.edu.my>
2022-04-22 09:59:15 +0800

    Added function to convert from letters to binary

--- InfoandSystem.py
+++ InfoandSystem.py
@@ -2,10 +2,22 @@
 from string import ascii_uppercase
 
 def kMap(size, equation):
+    pass
+
+def letter_to_binary(equation):
+    eq = ""
     for i in range(len(equation)):
         letter = equation[i]
         if letter in ascii_uppercase:
-            if(equation[i+1] == '\''):
-                print(False)
-            else:
-                print(True)
+            try:
+                if(equation[i+1] == '\''):
+                    letter = "0"
+                else:
+                    letter = "1"
+            except IndexError:
+                letter = "1"
+        else:
+            if letter == '\'':
+                letter = ""
+        eq = "".join([eq, letter])
+    return eq
