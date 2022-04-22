80b0a7b421f7c5a3609430510058af4632ad8bea
ecyht2 <ecyht2@nottingham.edu.my>
2022-04-22 09:15:25 +0800

    Added hp to w and rpm to rad support

--- PandE.py
+++ PandE.py
@@ -16,3 +16,19 @@ def imp_cap_rev(imp, freq):
 
 def imp_ind(ind, freq):
     return 2*pi*freq*ind*1j
+
+# Horse Power
+def hp_to_W(hp):
+    W = 746*hp
+    return W
+def W_to_hp(W):
+    hp = W/746
+    return hp
+
+#RPM
+def rpm_to_rad(rpm):
+    rad = rpm * 2*pi/60
+    return rad
+def rad_to_rpm(rad):
+    rpm = rad * 60/(2*pi)
+    return rpm
