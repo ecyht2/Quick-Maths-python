93461bed0f5bb3ccb7c46c412267f237136715bd
ecyht2 <ecyht2@nottingham.edu.my>
2022-04-25 09:25:13 +0800

    Added value for mu0

--- Constants.py
+++ Constants.py
@@ -2,6 +2,6 @@
 from math import pi
 # Constants
 epsilon0 = 8.85e-12     # Free space permittivity
-mu0 = 0                 # Free space
+mu0 = 1.26e-6           # Free space permeability 4*pi*10^-7
 k = 9e9                 # Coulomb's Constant 1/(4*pi*epsilon0)
 c = 3e6                 # Speed of Light in air/vacuum
