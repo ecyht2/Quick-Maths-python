21e0c9674538bd1bfd61a33669c0dfad5f662e95
ecyht2 <ecyht2@nottingham.edu.my>
2022-04-25 14:59:10 +0800

    Fixed bug with Vector mag function

--- Maths.py
+++ Maths.py
@@ -162,7 +162,7 @@ def mag(a = 0, b = 0, c = 0, vector = None) -> float:
         The magnitude of the Vector
     """
     if not vector == None:
-        if type(vector) == list or type(vector) == tuple or type(vector) == Vector:
+        if not (type(vector) == list or type(vector) == tuple or type(vector) == Vector):
             raise TypeError("Invalid vector passed in. It must be an array_like (list, tuple, Vector) object")
         if len(vector) > 3:
             raise ValueError("Invalid vector, a vector must include at most 3 values")
