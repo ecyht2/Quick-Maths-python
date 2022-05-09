e5e60faa172325d379c8fdbe109f0dda70915e0c
ecyht2 <ecyht2@nottingham.edu.my>
2022-05-09 22:35:44 +0800

    Fix Speed of Light

--- Constants.py
+++ Constants.py
@@ -6,7 +6,7 @@ from math import pi
 epsilon0 = 8.85e-12     # Free space permittivity
 mu0 = 1.26e-6           # Free space permeability 4*pi*10^-7
 kCoulomb = 9e9          # Coulomb's Constant 1/(4*pi*epsilon0)
-c = 3e6                 # Speed of Light in air/vacuum 1/(mu0*epsilon0)^0.5
+c = 3e8                 # Speed of Light in air/vacuum 1/(mu0*epsilon0)^0.5
 
 # Info and System
 kBolzman = 1.38e-23
