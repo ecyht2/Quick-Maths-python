a5e5e267547b89f9802c73f726342993b79ef135
ecyht2 <ecyht2@nottingham.edu.my>
2022-05-13 14:50:15 +0800

    Convolution uses np
    This reverts commit 3d9c74ff7792ece0c719ee216355dcbd173bddc6.

--- InfoandSystem.py
+++ InfoandSystem.py
@@ -244,50 +244,7 @@ def information_content(N: int, b: int = 0, P: float = 0) -> float:
     return H
 
 # Digital Filter
-def convolution(x: list, h: list) -> list:
-    """
-    Find the convolution of data stream x given impulse response h
-    Parameters
-    ----------
-    x
-        A list containing all the data of the data stream
-    h
-        A list containing all the impulse response
-    Returns
-    -------
-    list
-        The convoluted result
-    """
-    # Can be achieved via numpy.convolve(x, h)
-    result = []
-    row = []
-    xSize = len(x)
-    hSize = len(h)
-
-    # Finding y per sample
-    for rows in range(xSize):
-        # Appending leading 0
-        column = [0 for i in range(rows)]
-        # Finding output of x[n]
-        for i in h:
-            column.append(i * x[rows])
-        # Appending trailing 0
-        while len(column) < xSize + hSize - 1:
-            column.append(0)
-        # Appending row to column
-        row.append(column)
-
-    # Summing y per sample
-    for columns in range(xSize + hSize - 1):
-        # Summing the columns
-        total = 0
-        for rows in range(xSize):
-            total += row[rows][columns]
-        # Appending to rsult
-        result.append(total)
-
-    # Returning result
-    return result
+convolution = np.convolve
 
 def filter(b: list, a: list, x: list) -> list:
     """
