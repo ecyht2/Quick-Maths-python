9cc1e04e85acb9521325c74c96f4d86b0852f2bc
ecyht2 <ecyht2@nottingham.edu.my>
2022-12-25 14:14:58 +0800

    graph_error.py now checks '__main__' check

--- Examples/EngineeringMaths/graph_error.py
+++ Examples/EngineeringMaths/graph_error.py
@@ -34,109 +34,109 @@ def parse_arguments():
 
 
 if __name__ == '__main__':
-# Retriving values from parsed args
-args = parse_arguments()
-x_data = args.x_data
-y_data = args.y_data
-mode = args.mode
-xLabel = args.x_label
-yLabel = args.y_label
+    # Retriving values from parsed args
+    args = parse_arguments()
+    x_data = args.x_data
+    y_data = args.y_data
+    mode = args.mode
+    xLabel = args.x_label
+    yLabel = args.y_label
 
-# Reading x values
-xData = {}
-with open(x_data, "r", encoding="utf-8") as f:
-    line = f.readline()
-    while not line == '':
-        values = line.split(",")
-        xData[values[0]] = {"Raw Data": []}
-        for value in values[1:]:
-            value = value.strip()
-            try:
-                xData[values[0]]["Raw Data"].append(float(value))
-            except ValueError:
-                pass
+    # Reading x values
+    xData = {}
+    with open(x_data, "r", encoding="utf-8") as f:
         line = f.readline()
-# Reading y values
-yData = {}
-with open(y_data, "r", encoding="utf-8") as f:
-    line = f.readline()
-    while not line == '':
-        values = line.split(",")
-        yData[values[0]] = {"Raw Data": []}
-        for value in values[1:]:
-            value = value.strip()
-            try:
-                yData[values[0]]["Raw Data"].append(float(value))
-            except ValueError:
-                pass
+        while not line == '':
+            values = line.split(",")
+            xData[values[0]] = {"Raw Data": []}
+            for value in values[1:]:
+                value = value.strip()
+                try:
+                    xData[values[0]]["Raw Data"].append(float(value))
+                except ValueError:
+                    pass
+            line = f.readline()
+    # Reading y values
+    yData = {}
+    with open(y_data, "r", encoding="utf-8") as f:
         line = f.readline()
+        while not line == '':
+            values = line.split(",")
+            yData[values[0]] = {"Raw Data": []}
+            for value in values[1:]:
+                value = value.strip()
+                try:
+                    yData[values[0]]["Raw Data"].append(float(value))
+                except ValueError:
+                    pass
+            line = f.readline()
 
-# Getting Statistics of Raw Data
-for key in xData:
-    key["Stats"] = myStats(key.get("Raw Data"))
-for key in yData:
-    key["Stats"] = myStats(key["Raw Data"])
+    # Getting Statistics of Raw Data
+    for key in xData:
+        key["Stats"] = myStats(key.get("Raw Data"))
+    for key in yData:
+        key["Stats"] = myStats(key["Raw Data"])
 
-# Getting x, xerr, y and yerr values according to mode
-x = []
-xerr = []
-y = []
-yerr = []
-if mode == "STD":
-    # Looping over xData
-    for data in xData.values():
-        stats = data["Stats"]
-        x.append(stats.mean)
-        xerr.append(stats.std)
-    # Looping over yData
-    for data in yData.values():
-        stats = data["Stats"]
-        y.append(stats.mean)
-        yerr.append(stats.std)
-elif mode == "Range":
-    # Looping over xData
-    for data in xData.values():
-        stats = data["Stats"]
-        x.append(stats.median)
-        xerr.append(stats.range)
-    # Looping over yData
-    for data in yData.values():
-        stats = data["Stats"]
-        y.append(stats.median)
-        yerr.append(stats.range)
-elif mode == "Max-Min":
-    # Looping over xData
-    for data in xData.values():
-        stats = data["Stats"]
-        x.append(stats.median)
-        xerr.append([x[-1] - stats.min, stats.max - x[-1]])
-    # Looping over yData
-    for data in yData.values():
-        stats = data["Stats"]
-        y.append(stats.median)
-        yerr.append([y[-1] - stats.min, stats.max - y[-1]])
+    # Getting x, xerr, y and yerr values according to mode
+    x = []
+    xerr = []
+    y = []
+    yerr = []
+    if mode == "STD":
+        # Looping over xData
+        for data in xData.values():
+            stats = data["Stats"]
+            x.append(stats.mean)
+            xerr.append(stats.std)
+        # Looping over yData
+        for data in yData.values():
+            stats = data["Stats"]
+            y.append(stats.mean)
+            yerr.append(stats.std)
+    elif mode == "Range":
+        # Looping over xData
+        for data in xData.values():
+            stats = data["Stats"]
+            x.append(stats.median)
+            xerr.append(stats.range)
+        # Looping over yData
+        for data in yData.values():
+            stats = data["Stats"]
+            y.append(stats.median)
+            yerr.append(stats.range)
+    elif mode == "Max-Min":
+        # Looping over xData
+        for data in xData.values():
+            stats = data["Stats"]
+            x.append(stats.median)
+            xerr.append([x[-1] - stats.min, stats.max - x[-1]])
+        # Looping over yData
+        for data in yData.values():
+            stats = data["Stats"]
+            y.append(stats.median)
+            yerr.append([y[-1] - stats.min, stats.max - y[-1]])
 
-    # Transposing errors
-    # x
-    xerr = np.array(xerr)
-    xerr = xerr.transpose()
-    # y
-    yerr = np.array(yerr)
-    yerr = yerr.transpose()
-elif mode == "None":
-    # Looping over xData
-    for data in xData.values():
-        stats = data["Stats"]
-        x.append(stats.mean)
-        xerr.append(0)
-    # Looping over yData
-    for data in yData.values():
-        stats = data["Stats"]
-        y.append(stats.mean)
-        yerr.append(0)
+        # Transposing errors
+        # x
+        xerr = np.array(xerr)
+        xerr = xerr.transpose()
+        # y
+        yerr = np.array(yerr)
+        yerr = yerr.transpose()
+    elif mode == "None":
+        # Looping over xData
+        for data in xData.values():
+            stats = data["Stats"]
+            x.append(stats.mean)
+            xerr.append(0)
+        # Looping over yData
+        for data in yData.values():
+            stats = data["Stats"]
+            y.append(stats.mean)
+            yerr.append(0)
 
-print(x)
-print(y)
-# Plotting data
-plot_error_bar({"x": x, "y": y}, {"yerr": yerr, "xerr": xerr},
-               {"xlabel": xLabel, "ylabel": yLabel})
+    print(x)
+    print(y)
+    # Plotting data
+    plot_error_bar({"x": x, "y": y}, {"yerr": yerr, "xerr": xerr},
+                   {"xlabel": xLabel, "ylabel": yLabel})
