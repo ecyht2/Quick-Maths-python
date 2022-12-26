068528912c1316a9a062a319b11474c37eaf1a47
ecyht2 <ecyht2@nottingham.edu.my>
2022-12-26 09:57:11 +0800

    Updated time area rule equations

--- src/eeepy/year_2/ecc/power_electronics.py
+++ src/eeepy/year_2/ecc/power_electronics.py
@@ -2,41 +2,110 @@
 """Functions related to Power Electronics."""
 
 
-class TimeAreaRule(float):
-    """Time Area Rule Equations (dI = VTA/L, dV = ITA/C)."""
-    def __new__(cls, area: float, value: float):
-        """Find the change in I/V
+class TimeAreaRuleL(float):
+    """Time Area Rule equation for an inductor. (dI = VTA/L)"""
+    def __new__(cls, area: float, L: float):
+        """Find the ΔI of a given VTA and L.
 
         Parameters
         ----------
         area: float
-            Time Area could be VTA (voltage time area) or
-            ITA (current time area)
-        value: float
-            The capcitance or inductance
+            The time Area VTA. (voltage time area)
+        L: float
+            The inductance.
         """
-        return super().__new__(cls, area / value)
+        return super().__new__(cls, area / L)
 
     def __init__(self, area: float, value: float):
-        """Find the change in I/V
+        """Find the ΔI of a given VTA and L.
 
         Parameters
         ----------
         area: float
-            Time Area could be VTA (voltage time area) or
-            ITA (current time area)
-        value: float
-            The capcitance or inductance
+            The time Area VTA. (voltage time area)
+        L: float
+            The inductance.
         """
         super().__init__()
 
-    @classmethod
-    def from_inductor(cls, VTA, L):
-        return cls(VTA, L)
+    @staticmethod
+    def find_inductance(VTA: float, dI: float):
+        """Find the inductance needed for a given VTA and ΔI. (L = VTA / ΔI)
 
-    @classmethod
-    def from_capacitor(cls, ITA, C):
-        return cls(ITA, C)
+        Prameters
+        ---------
+        VTA: float
+            The VTA. (voltage time area)
+        dI: float
+            The change in current ΔI.
+        """
+        return VTA / dI
+
+    @staticmethod
+    def find_VTA(dI: float, L: float):
+        """Find the VTA for a given ΔI and L. (VTA = ΔI * L)
+
+        Parameters
+        ----------
+        dI: float
+            The change in current ΔI.
+        L: float
+            The inductance.
+        """
+        return dI * L
+
+
+class TimeAreaRuleC(float):
+    """Time Area Rule equation for an capacitor. (dV = ITA/C)"""
+    def __new__(cls, area: float, C: float):
+        """Find the ΔV of a given ITA and C.
+
+        Parameters
+        ----------
+        area: float
+            The ITA. (current time area)
+        C: float
+            The capacitance.
+        """
+        return super().__new__(cls, area / C)
+
+    def __init__(self, area: float, value: float):
+        """Find the ΔV of a given ITA and C.
+
+        Parameters
+        ----------
+        area: float
+            The ITA. (current time area)
+        C: float
+            The capacitance.
+        """
+        super().__init__()
+
+    @staticmethod
+    def find_capacitance(ITA: float, dV: float):
+        """Find the capacitance needed for a given ITA and ΔV. (C = ITA / ΔV)
+
+        Prameters
+        ---------
+        ITA: float
+            The ITA. (current time area)
+        dV: float
+            The change in voltage ΔV.
+        """
+        return ITA / dV
+
+    @staticmethod
+    def find_ITA(dV: float, C: float):
+        """Find the ITA for a given ΔV and C. (ITA = ΔV * C)
+
+        Parameters
+        ----------
+        dV: float
+            The change in voltage ΔV.
+        C: float
+            The capacitance.
+        """
+        return dV * C
 
 
 def average_voltage(duty_cycle: float, voltage: float) -> float:
