5bb4416ea880533ac0690e651e4ae6b597a402d2
ecyht2 <ecyht2@nottingham.edu.my>
2023-01-13 15:54:41 +0800

    Fixed too-many-locals

--- Examples/year_2/ecc/losses_example.py
+++ Examples/year_2/ecc/losses_example.py
@@ -20,59 +20,81 @@ def run():
     f = 200e3
 
     # MOSFET
+    i = {}
     # i_q = (i_t1 + i_p1) / 2 * d
-    i_q_rms = math.sqrt(d * (i_p1**2 + i_p1 * i_t1 + i_t1**2) / 3)
-    i_q_rms = power_devices_losses.I_RMS(i_p1, i_t1, d)
+    i["q"] = {}
+    i["q"]["rms"] = math.sqrt(d * (i_p1**2 + i_p1 * i_t1 + i_t1**2) / 3)
+    i["q"]["rms"] = power_devices_losses.I_RMS(i_p1, i_t1, d)
 
     # D
-    i_d = (i_t + i_p) / 2 * (1 - d)
-    i_d_rms = math.sqrt((1 - d) * (i_p**2 + i_p * i_t + i_t**2) / 3)
+    i["d"] = {}
+    i["d"]["avg"] = (i_t + i_p) / 2 * (1 - d)
+    i["d"]["rms"] = math.sqrt((1 - d) * (i_p**2 + i_p * i_t + i_t**2) / 3)
 
     # D3
-    i_d3 = (i_t + i_p) / 2 * d
-    i_d3_rms = math.sqrt(d * (i_p**2 + i_p * i_t + i_t**2) / 3)
+    i["d3"] = {}
+    i["d3"]["avg"] = (i_t + i_p) / 2 * d
+    i["d3"]["rms"] = math.sqrt(d * (i_p**2 + i_p * i_t + i_t**2) / 3)
 
     # Loss numbers
     # MOSFET
-    R_q = 120e-3
-    t_on = 50e-9
-    t_off = 100e-9
+    mosfet = {}
+    mosfet["R_q"] = 120e-3
+    mosfet["t_on"] = 50e-9
+    mosfet["t_off"] = 100e-9
 
     # diodes
-    R_d = (1.7 - 0.7) / 20
-    V_on_d = 0.7
+    diodes = {}
+    diodes["R_d"] = (1.7 - 0.7) / 20
+    diodes["V_on_d"] = 0.7
 
     # Conduction losses
-    cl_q = R_q * i_q_rms**2
-    cl_q_2 = power_devices_losses.ConductionLoss.mosfet(R_q, i_q_rms)
-    cl_d = i_d * V_on_d + R_d * i_d_rms**2
-    cl_d_2 = power_devices_losses.ConductionLoss.diode(V_on_d, i_d, R_d,
-                                                       i_d_rms)
-    cl_d3 = i_d3 * V_on_d + R_d * i_d3_rms**2
-    cl_d3_2 = power_devices_losses.ConductionLoss.diode(V_on_d, i_d3, R_d,
-                                                        i_d3_rms)
+    conduction_loss = {}
+    conduction_loss["cl_q"] = mosfet["R_q"] * i["q"]["rms"]**2
+    conduction_loss["cl_q_2"] = power_devices_losses.ConductionLoss.mosfet(
+        mosfet["R_q"], i["q"]["rms"]
+    )
+    conduction_loss["cl_d"] = i["d"]["avg"] * diodes["V_on_d"] +\
+        diodes["R_d"] * i["d"]["rms"]**2
+    conduction_loss["cl_d_2"] = power_devices_losses.ConductionLoss.diode(
+        diodes["V_on_d"], i["d"]["avg"], diodes["R_d"], i["d"]["rms"]
+    )
+    conduction_loss["cl_d3"] = i["d3"]["avg"] * diodes["V_on_d"] +\
+        diodes["R_d"] * i["d3"]["rms"]**2
+    conduction_loss["cl_d3_2"] = power_devices_losses.ConductionLoss.diode(
+        diodes["V_on_d"], i["d3"]["avg"], diodes["R_d"], i["d3"]["rms"]
+    )
     print("Conduction Loss:")
-    print(f"Q: {cl_q}, {cl_q_2}")
-    print(f"D: {cl_d}, {cl_d_2}")
-    print(f"D3: {cl_d3}, {cl_d3_2}")
+    print(f"Q: {conduction_loss['cl_q']}, {conduction_loss['cl_q_2']}")
+    print(f"D: {conduction_loss['cl_d']}, {conduction_loss['cl_d_2']}")
+    print(f"D3: {conduction_loss['cl_d3']}, {conduction_loss['cl_d3_2']}")
 
     # Switching losses
+    switching_loss = {}
     # ON
-    sl_on = 0.5 * i_t1 * t_on * V_s * f
-    sl_on_2 = power_devices_losses.SwitchingLoss.power_turn_on(i_t1, V_s,
-                                                               t_on, f)
+    switching_loss["sl_on"] = 0.5 * i_t1 * mosfet["t_on"] * V_s * f
+    switching_loss["sl_on_2"] =\
+        power_devices_losses.SwitchingLoss.power_turn_on(
+            i_t1, V_s, mosfet["t_on"], f
+    )
     # OFF
-    sl_off = 0.5 * i_p1 * t_off * V_s * f
-    sl_off_2 = power_devices_losses.SwitchingLoss.power_turn_off(i_p1, V_s,
-                                                                 t_off, f)
-    print(f"On loss: {sl_on}, {sl_on_2}")
-    print(f"Off loss: {sl_off}, {sl_off_2}")
+    switching_loss["sl_off"] = 0.5 * i_p1 * mosfet["t_off"] * V_s * f
+    switching_loss["sl_off_2"] =\
+        power_devices_losses.SwitchingLoss.power_turn_off(
+            i_p1, V_s, mosfet["t_off"], f
+    )
+    print(f"On loss: {switching_loss['sl_on']}, {switching_loss['sl_on_2']}")
+    print(f"Off loss: {switching_loss['sl_off']}, "
+          f"{switching_loss['sl_off_2']}")
 
     # Efficiency
-    P = V_o * I_o
-    P_loss = 2 * cl_q + cl_d + cl_d3 + sl_on + sl_off
-    eff = P / (P + P_loss)
-    print(f"Efficiency: {eff:%}")
+    power = {}
+    power["in"] = V_o * I_o
+    power["loss"] = 2 * conduction_loss["cl_q"] + conduction_loss["cl_d"] +\
+        conduction_loss["cl_d3"] + switching_loss["sl_on"] +\
+        switching_loss["sl_off"]
+    power["eff"] = power["in"] / (power["in"] + power["loss"])
+    print(f"Efficiency: {power['eff']:%}")
 
 
 if __name__ == '__main__':
