26634cc98d0f27d05a0aeb0165005d73511e4eca
ecyht2 <ecyht2@nottingham.edu.my>
2023-02-09 16:37:59 +0800

    Added coordinate_system

--- src/eeepy/year_2/mmt/coordinate_system.py
+++ src/eeepy/year_2/mmt/coordinate_system.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 """Functions and Equations that helps with calculating values related to
  coordinate system."""
+
+
+class Point:
+    def __init__(self, x: float, y: float):
+        self.x = float(x)
+        self.y = float(y)
+
+    def from_polar(cls, r: float, phi: float):
+        return cls(0, 0, 0)
+
+
+class Point3D:
+    def __init__(self):
+        ...
