651b5d7492ec04e7ecebad568e21f0e42e5ea95a
ecyht2 <ecyht2@nottingham.edu.my>
2023-03-19 22:46:29 +0800

    Added 3DPoint

--- src/eeepy/year_2/mmt/coordinate_system.py
+++ src/eeepy/year_2/mmt/coordinate_system.py
@@ -73,5 +73,96 @@ class Point:
 
 
 class Point3D:
-    def __init__(self):
-        ...
+    """A Point in a 3D plane."""
+    def __init__(self, x: float, y: float, z: float):
+        """Creates a Point in a 2D plane.
+
+        :param x: The x coordinate of the point.
+        :param y: The y coordinate of the point.
+        :param z: The z coordinate of the point.
+        """
+        self._x = float(x)
+        self._y = float(y)
+        self._z = float(z)
+
+    @property
+    def x(self):
+        """The x coordinate of the point."""
+        return self._x
+
+    @property
+    def y(self):
+        """The y coordinate of the point."""
+        return self._y
+
+    @property
+    def z(self):
+        """The z coordinate of the point."""
+        return self._z
+
+    @property
+    def r_spherical(self):
+        """The distance from origin."""
+        return math.sqrt(self.x**2 + self.y**2 + self.z**2)
+
+    @property
+    def theta(self):
+        """The zenith angle of the point."""
+        return math.acos(self.z / self.r_spherical)
+
+    @property
+    def phi(self):
+        """The azimuthal angle of the point."""
+        return math.atan2(self.y, self.x)
+
+    @property
+    def r_cylindrical(self):
+        """The radial distance of the point."""
+        return math.sqrt(self.x**2 + self.y**2)
+
+    def cartesian(self) -> tuple[float, float, float]:
+        """The Point in cartesian form.
+
+        :return: (x, y, z)
+        """
+        return (self.x, self.y, self.z)
+
+    def spherical(self) -> tuple[float, float, float]:
+        """The Point in spherical form.
+
+        :return: (r, ɸ, θ)
+        """
+        return (self.r_spherical, self.phi, self.theta)
+
+    def cylindrical(self) -> tuple[float, float, float]:
+        """The Point in cylindrical form.
+
+        :return: (r, ɸ, z)
+        """
+        return (self.r_cylindrical, self.phi, self.z)
+
+    @classmethod
+    def from_spherical(cls, r: float, phi: float, theta: float):
+        """Creates a Point in a 3D plane from spherical coordinates.
+
+        :param r: The distance from origin.
+        :param phi: The azimuthal angle of the point.
+        :param theta: The zenith angle of the point.
+        """
+        x = r * math.sin(theta) * math.cos(phi)
+        y = r * math.sin(theta) * math.sin(phi)
+        z = r * math.cos(theta)
+        return cls(x, y, z)
+
+    @classmethod
+    def from_cylindrical(cls, r: float, phi: float, z: float):
+        """Creates a Point in a 3D plane from cylindrical coordinates.
+
+        :param r: The radial distance of the point.
+        :param phi: The azimuth angle of the point.
+        :param z: The z coordinate of the point.
+        """
+        x = r * math.cos(phi)
+        y = r * math.sin(phi)
+        z = z
+        return cls(x, y, z)
