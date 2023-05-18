b7b8231615a6a28d09a2530e9bf4d49edb70cbc6
ecyht2 <ecyht2@nottingham.edu.my>
2023-05-18 11:44:49 +0800

    Reexported modules in numerical_integration

--- src/eeepy/year_2/mmt/numerical_integration/__init__.py
+++ src/eeepy/year_2/mmt/numerical_integration/__init__.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3
 """Functions and Equations relating to numerical integration."""
+from .integration import riemann_sum, simpson_13, simpson_38, trapezoid_rule
+from .ode import eulers_forward, heun_method, rk4_method
+__all__ = [
+    "riemann_sum",
+    "simpson_13",
+    "simpson_38",
+    "trapezoid_rule",
+    "eulers_forward",
+    "heun_method",
+    "rk4_method",
+]
